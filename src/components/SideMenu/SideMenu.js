145d9ec4ebdeaf7d98e78d3dfde8b1a8139663ab
Steve Rayner <stephen.rayner@marmalade.co.uk>
2018-08-03 14:00:05 +0100

    added event handlers

--- src/components/SideMenu/SideMenu.js
+++ src/components/SideMenu/SideMenu.js
@@ -2,11 +2,25 @@ import React, { Component } from 'react';
 import styles from './SideMenu.css';
 
 class SideMenu extends Component {
+
+    state = {
+        active: this.props.items[0].text
+    }
+
+    itemClicked = (item) => {
+        console.log('Clicked');
+        this.setState({active: item.text})
+    }
+
+    toggleClicked = () => {
+        console.log('toggle clicked');
+    }
+
     render() {
         const menuItems = this.props.items.map(item => {
             return (
-                <li className={this.props.active === item.text ? 'active' : ''}>
-                    <a href="#">
+                <li className={this.state.active === item.text ? 'active' : ''}>
+                    <a href="#" onClick={() => this.itemClicked(item)}>
                         <i className={item.icon + ' fa-lg fa-fw'}></i>
                         <span className="text">{item.text}</span>
                     </a>
@@ -17,7 +31,7 @@ class SideMenu extends Component {
         return (
             <nav id="side-menu" className="side-menu">
                 <ul>{menuItems}</ul>
-                <div id="side-menu-toggle">
+                <div onClick={() => this.toggleClicked()} id="side-menu-toggle">
                     <i id="side-menu-toggle-icon" class="fas fa-chevron-right fa-lg fa-fw"></i>
                 </div>
             </nav>

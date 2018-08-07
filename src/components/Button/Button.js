2cc4650f5386e7ffb5a5e70921da92f95f454172
Steve Rayner <srayner02@gmail.com>
2018-08-07 08:57:14 +0100

    added primary button styles

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -2,8 +2,14 @@ import React from 'react';
 import './Button.css';
 
 const Button = (props) => {
+
+    let className = 'Button';
+    if (props.hasOwnProperty('type')) {
+        className = className + ' ' + props.type;
+    }
+
     return (
-        <button className="Button">
+        <button className={className}>
             {props.text}
         </button>
     );
--- src/pages/Settings.js
+++ src/pages/Settings.js
@@ -1,18 +1,20 @@
 import React from 'react';
 import Card from '../components/Card/Card';
 import Input from '../components/Input/Input';
+import Button from '../components/Button/Button';
 
 const Settings = (props) => {
     return (
-        <div>
-            <h2>Settings</h2>
-            
+        <div className="grid">
+            <div>
+                <h2>Settings</h2>
                 <form>
                     <Input placeholder="fred" id='form-username' name="username" label="Username"/>
                     <Input id='form-department' name="department" label="Department"/>
                     <Input prepend="£" append=".00" id='form-salary' name="salary" label="Salary"/>
+                    <Button type="primary" text="Add"/>
                 </form>
-            
+            </div>
         </div>
     )
 }
